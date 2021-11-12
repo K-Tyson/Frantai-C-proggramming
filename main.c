af303d66b0e2b26307c1e9f66551d7df30da42f9
K-Tyson <91784580+K-Tyson@users.noreply.github.com>
2021-11-12 10:38:18 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+long area(long radius);
+long input,answer;
+int main(void)
+{
+printf("enter an integer value");
+scanf("%ld",&input);
+answer=area(input);
+printf("\n the area of %ld is %ld",input,answer);
+return 0;
+}
+long area(long radius)
+{
+long area,pi;
+pi=3.142;
+area=pi*radius*radius;
+return area;
+}
+
+
